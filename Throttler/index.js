f095cc17711e602f5ff8942bf2d26a9ed12b9914
BhaveshSuthar00 <bhaveshs0770@gmail.com>
2022-04-27 15:39:48 +0530

    added the toggler function

--- Throttler/index.js
+++ Throttler/index.js
@@ -1,12 +1,19 @@
-let id;
 function tog(){
-    alert('hellow world')
+    let count = document.getElementById('count').textContent || 0
+    count = +count + 1;
+    console.log(count)
+    document.getElementById('count').textContent = count;
 }
+let timerId;
 function toggler(func, delay) {
-    if(id){
-        clearInterval(id)
-    }
-    id = setTimeout(() => {
-        func()
-    }, delay);
+    if (timerId) {
+		return
+	}
+	timerId  =  setTimeout(function () {
+		func()
+		timerId  =  undefined;
+	}, delay)
 }
+document.getElementById('btn').addEventListener('click',function(){
+    toggler(tog, 1000)
+})
